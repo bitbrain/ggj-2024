a319a1f55285f86c19bc6fe103b27d860bede121
bitbrain <miguel-gonzalez@gmx.de>
2024-01-27 13:47:53 +0000

    make smiley happy

--- godot/world/player/player.gd
+++ godot/world/player/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var MAX_SPEED = 275
 
 
+@onready var player_sprite: AnimatedSprite2D = $PlayerSprite
 @onready var endorphin_detector: Area2D = $EndorphinDetector
 @onready var collectors: Array[Node] = $Collectors.get_children()
 
@@ -17,10 +18,20 @@ var endorphins := 0:
 		if collectors != null:
 			for index in range(0, collectors.size()):
 				var collector = collectors[index] as Collector
-				collector.charged = index <= endorphins
-
+				collector.charged = index < endorphins
+var happiness := 0:
+	set(h):
+		happiness = h
+		if player_sprite != null:
+			if happiness > 3:
+				player_sprite.animation = "happy"
+			elif happiness < 0:
+				player_sprite.animation = "sad"
+			else:
+				player_sprite.animation = "neutral"
 
 func _ready() -> void:
+	self.happiness = 0
 	endorphin_detector.body_entered.connect(_on_endorphin_collected)
 
 
@@ -28,7 +39,10 @@ func _on_endorphin_collected(endorphin:Endorphin) -> void:
 	endorphins += 1
 	# TODO: animate absorbtion process
 	endorphin.queue_free()
-
+	
+	if endorphins == collectors.size():
+		endorphins = 0
+		happiness += 1
 
 func move(input_vector:Vector2) -> void:
 	self.input_vector = input_vector.normalized()
